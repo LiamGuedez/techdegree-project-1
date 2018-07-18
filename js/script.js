d95daaca0a682198573f4a1d76d15b3f2eea4e3b
LiamGuedez <liamhayes9393@gmail.com>
2018-07-18 14:07:46 -0400

    Finished the basic requirements for the project
    *I still have to do the extra credit.
    
    *And I have to fix one thing. This website always displays this quote first:
    
    "You can do anything but not everything."
    
    Then after that, every quote is random. That's not right, every quote should be random, even the first one.

--- js/script.js
+++ js/script.js
@@ -2,3 +2,59 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+//1. Create an array of JavaScript objects to hold the data for your quotes.
+//2. Each quote object in the quotes array should have the following properties:
+//   quote, source, optional citation, and optional year.
+// an array of quotes.
+var quotes =
+[
+    {quote: "Cheese is good", source: "A guy that loves cheese"},
+    {quote: "Cake is amazing", source: "A girl that loves cake"},
+    {quote: "There is nothing like a citation", source: "A dude that loves ciations", citation: "citation magazine"},
+    {quote: "It's best to have everything, so steal it", source: "A cleptomaniac chick", citation: "South Carolina Encyclopedia of Violent Crimes", year: "2018"}
+];
+
+//3. Create a function named getRandomQuote.
+// returns a random quote object from the quotes array.
+function getRandomQuote()
+{
+  var randomIndex = Math.floor(Math.random() * quotes.length) + 0;
+  return quotes[randomIndex];
+}
+
+//4. Create a function named printQuote.
+// constructs a string containing the different properties of the quote object.
+// displays the final HTML string to the page.
+function printQuote()
+{
+  var quoteObject = getRandomQuote();
+  var html = '<p class="quote">' + quoteObject.quote + '</p>';
+  html += '<p class="source">' + quoteObject.source;
+
+  if(quoteObject.citation)
+  {
+    html += '<span class="citation">' + quoteObject.citation + '</span>';
+  }
+
+  if(quoteObject.year)
+  {
+    html += '<span class="year">' + quoteObject.year + '</span>';
+  }
+
+  html += "</p>";
+  document.getElementById('quote-box').innerHTML = html;
+}
+
+//5. Add good code comments to your JavaScript code.
+
+//6. If you're having trouble with this project,
+//make sure you take a look at this great study guide: Random Quote Generator Study Guide
+
+//7. Before you submit your project for review,
+//make sure you can check off all of the items on the Student Project Submission Checklist.
+
+//Extra Credit 1. Add more properties to the quote object.
+
+//Extra Credit 2. Random background color.
+
+//Extra Credit 3. Auto-refresh the quote.
